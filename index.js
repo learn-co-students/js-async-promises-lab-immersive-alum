c90b135969acc79dfbc46f1dd714dde73a3287ce
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2018-07-10 22:40:00 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,49 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion(question){
+  document.querySelector('.question-container').innerHTML = question.questionText;
+}
+
+function askQuestionThen(time){
+  question = questions[0];
+  return new Promise((resolve, reject) => {
+    setTimeout(function(){
+      appendQuestion(question)
+      resolve(null)
+    }, time )
+  })
+}
+
+function removeQuestion(){
+  document.querySelector('.question-container').innerHTML = '';
+}
+
+function askQuestionThenRemoveQuestion(time){
+  appendQuestion(question);
+  return new Promise((resolve, reject) => {
+    setTimeout(function(){
+      removeQuestion();
+      toggleTrueAndFalseButtons()
+      resolve(null)
+    }, time )
+  })
+}
+
+function trueAndFalseButtons(){
+  return document.querySelectorAll('.true-false-list .btn');
+}
+
+function toggleTrueAndFalseButtons(){
+  trueAndFalseButtons().forEach(el => el.classList.toggle("hide"));
+}
+
+function displayQuestionOnClick(){
+  askQuestionThenRemoveQuestion(5000);
+}
+
+let button = document.querySelector('#ask')
+button.addEventListener("click", function(){
+  displayQuestionOnClick();
+})
